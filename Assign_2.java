52b27c03d0f2aabbd23fb76b17c8b5d31db92e19
Snehak1 <be_sneha_singh@outlook.com>
2017-06-20 16:21:44 +0530

    Add files via upload

---
+++ Assign_2.java
@@ -0,0 +1,21 @@
+
+public class Assign_2 {
+
+	
+	public static void main(String[] args) {
+	
+		int a=20;
+		int b=10;
+		int x= a-- - --a;
+		int y=a--;
+		int z=a>>2;
+		int u=a&b;
+		
+		System.out.println("a-- - --a =" + " "+ x);
+		System.out.println("a-- =" + " "+ y);
+		System.out.println("a>>2 =" + z);
+		System.out.println("a&b =" + u);
+		
+	}
+
+}
